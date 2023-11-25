40240c55e5fe6ac7c8756ae04c66a0b9d4c5c500
Richtermnd <m.rustam1305@gmail.com>
2023-11-25 08:07:44 +0500

    Tests logic + tests fot tests (wtf?).

--- pkg/tests/tests.go
+++ pkg/tests/tests.go
@@ -1,1 +1,77 @@
 package tests
+
+import (
+	"reflect"
+	"slices"
+	"strings"
+)
+
+// Question of Test
+type Question struct {
+	N             int      `json:"n"`             // Number of question
+	Text          string   `json:"text"`          // Text of question.
+	Variants      []string `json:"variants"`      // Variants of anwers.
+	Answers       []string `json:"answers"`       // Correct answers.
+	IsMultiAnswer bool     `json:"isMultiAnswer"` // Is question a multianswer. It's usefull for frontend.
+}
+
+// Test
+type Test struct {
+	Title     string     `json:"title"`     // Test title.
+	AuthorId  string     `json:"authorID"`  // Author id.
+	Questions []Question `json:"questions"` // Questions slice.
+}
+
+// Return all answers for test.
+func (t *Test) GetAnswers() *QuestionAnswers {
+	// Create copy original questions slice.
+	questionsCopy := make([]Question, len(t.Questions))
+	copy(questionsCopy, t.Questions)
+
+	// Get answers from questions.
+	answers := make([]*QuestionAnswer, len(questionsCopy))
+	for i, question := range questionsCopy {
+		answers[i] = &QuestionAnswer{
+			N:       question.N,
+			Answers: question.Answers,
+		}
+	}
+
+	// Convert answers to QuestionAnswers.
+	// return QuestionAnswers(answers) not work.
+	res := make(QuestionAnswers, len(answers))
+	copy(res, answers)
+	return &res
+}
+
+type QuestionAnswer struct {
+	N       int
+	Answers []string
+}
+
+type QuestionAnswers []*QuestionAnswer
+
+// Sort by nubmer
+// !!! Change source
+func (a *QuestionAnswers) Sort() *QuestionAnswers {
+	slices.SortFunc(*a, func(a, b *QuestionAnswer) int {
+		if a.N < b.N {
+			return -1
+		}
+		if a.N > b.N {
+			return 1
+		}
+		return 0
+	})
+	// Sorting answers
+	for _, answer := range *a {
+		slices.SortFunc(answer.Answers, strings.Compare)
+	}
+	return a
+}
+
+func (a *QuestionAnswers) Check(test Test) bool {
+	a.Sort()
+	correctAnswers := test.GetAnswers().Sort()
+	return reflect.DeepEqual(a, correctAnswers)
+}
---
+++ test/TestingSystem/tests_test.go
@@ -0,0 +1,126 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/Richtermnd/TestingSystem/pkg/tests"
+)
+
+func TestCheck(t *testing.T) {
+	// Most cursed thing that I write in Go.
+	testCases := []struct {
+		Name         string
+		Test         tests.Test
+		InputAnswers tests.QuestionAnswers
+		Expected     bool
+	}{
+		{
+			Name: "Correct test",
+			Test: tests.Test{
+				Title:    "Test1",
+				AuthorId: "",
+				Questions: []tests.Question{
+					{
+						N:             1,
+						Text:          "",
+						Variants:      []string{"a", "b", "c", "d"},
+						Answers:       []string{"a"},
+						IsMultiAnswer: false,
+					},
+					{
+						N:             2,
+						Text:          "",
+						Variants:      []string{"a", "b", "c", "d"},
+						Answers:       []string{"a", "b"},
+						IsMultiAnswer: true,
+					},
+				},
+			},
+			InputAnswers: tests.QuestionAnswers{
+				&tests.QuestionAnswer{
+					N:       1,
+					Answers: []string{"a"},
+				},
+				&tests.QuestionAnswer{
+					N:       2,
+					Answers: []string{"a", "b"},
+				},
+			},
+			Expected: true,
+		},
+		{
+			Name: "Uncorrect test",
+			Test: tests.Test{
+				Title:    "Test1",
+				AuthorId: "",
+				Questions: []tests.Question{
+					{
+						N:             1,
+						Text:          "",
+						Variants:      []string{"a", "b", "c", "d"},
+						Answers:       []string{"a"},
+						IsMultiAnswer: false,
+					},
+					{
+						N:             2,
+						Text:          "",
+						Variants:      []string{"a", "b", "c", "d"},
+						Answers:       []string{"a", "b"},
+						IsMultiAnswer: true,
+					},
+				},
+			},
+			InputAnswers: tests.QuestionAnswers{
+				&tests.QuestionAnswer{
+					N:       1,
+					Answers: []string{"b"},
+				},
+				&tests.QuestionAnswer{
+					N:       2,
+					Answers: []string{"a", "c"},
+				},
+			},
+			Expected: false,
+		},
+		{
+			Name: "Unordered answers test",
+			Test: tests.Test{
+				Title:    "Test1",
+				AuthorId: "",
+				Questions: []tests.Question{
+					{
+						N:             1,
+						Text:          "",
+						Variants:      []string{"a", "b", "c", "d"},
+						Answers:       []string{"a"},
+						IsMultiAnswer: false,
+					},
+					{
+						N:             2,
+						Text:          "",
+						Variants:      []string{"a", "b", "c", "d"},
+						Answers:       []string{"a", "b"},
+						IsMultiAnswer: true,
+					},
+				},
+			},
+			InputAnswers: tests.QuestionAnswers{
+				&tests.QuestionAnswer{
+					N:       2,
+					Answers: []string{"b", "a"},
+				},
+				&tests.QuestionAnswer{
+					N:       1,
+					Answers: []string{"a"},
+				},
+			},
+			Expected: true,
+		},
+	}
+	for _, test := range testCases {
+		res := test.InputAnswers.Check(test.Test)
+		if res != test.Expected {
+			t.Errorf("%s failed", test.Name)
+		}
+	}
+}
